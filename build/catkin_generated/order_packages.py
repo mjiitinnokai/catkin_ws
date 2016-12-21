97e0dcb6814105a872439b3500bc053e6f791220
muaz <muaz@muaz-GP60-2OD.(none)>
2016-12-21 22:33:41 +0800

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/muaz/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/muaz/catkin_ws/devel;/opt/ros/hydro".split(';') if "/home/muaz/catkin_ws/devel;/opt/ros/hydro" != "" else []
